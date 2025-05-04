e9c2a40ec8425bd6911fa4c6b2e9289a5e241ece
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-05-04 14:46:37 +0600

    Contact us added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,19 @@
-import React from "react";
 import Banner from "../../Components/Home/Banner";
 import About from "../../Components/Home/About";
 import Services from "../../Components/Home/Services";
 import Opportunities from "../../Components/Shared/Opportunities/Opportunities";
-import Review from "../../Components/Home/Review";
+import Review from "../../Components/Shared/Review/Review";
 
 const Home = () => {
-	return (
-		<div>
-			<Banner />
-			<About />
-			<Services />
-			<Review />
-			<Opportunities />
-		</div>
-	);
+  return (
+    <div>
+      <Banner />
+      <About />
+      <Services />
+      <Review />
+      <Opportunities />
+    </div>
+  );
 };
 
 export default Home;
