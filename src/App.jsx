b429f410ad5ee23831ea9f07fad04c4d16cb2e06
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-04 15:48:24 +0600

    made banner and about responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import Navbar from "./Components/Shared/Navbar";
 import Footer from "./Components/Shared/Footer";
 
 function App() {
-  return (
-    <div className="container mx-auto mt-7">
-      {/* Nav bar */}
-      <Navbar></Navbar>
+	return (
+		<div className="container mx-auto mt-7 font-inter">
+			{/* Nav bar */}
+			<Navbar></Navbar>
 
-      {/* Children component practice */}
-      <div className="min-h-[calc(100vh-196px)] ">
-        <Outlet />
-      </div>
+			{/* Children component practice */}
+			<div className="min-h-[calc(100vh-196px)] ">
+				<Outlet />
+			</div>
 
-      {/* Footer */}
-      <Footer></Footer>
-    </div>
-  );
+			{/* Footer */}
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;
