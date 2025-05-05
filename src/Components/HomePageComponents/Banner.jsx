fe86fa3d19557640324862007de34010a3061e2f
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-05 13:43:24 +0600

    added link system in Navbar

--- src/Components/HomePageComponents/Banner.jsx
+++ src/Components/HomePageComponents/Banner.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import bannerImage from "/assets/cargo.png";
 
 const Banner = () => {
@@ -5,7 +6,7 @@ const Banner = () => {
 
 	return (
 		<div className="w-full my-4 sm:my-6 md:my-8 lg:my-10 px-2 sm:px-4">
-			<div className="relative  z-0 overflow-hidden">
+			<div className="relative z-0 overflow-hidden">
 				{/* Heading */}
 				<div className="absolute top-3 left-3 xs:top-4 xs:left-4 sm:top-6 sm:left-6 md:top-8 md:left-8 lg:top-10 lg:left-10 z-10">
 					<h1 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl space-y-0 xs:space-y-1 sm:space-y-2">
@@ -22,7 +23,7 @@ const Banner = () => {
 					className="backdrop-blur-3xl w-full h-auto object-cover"
 				/>
 
-				{/* Bottom bar - Single responsive layout that wraps naturally */}
+				{/* Bottom bar with links */}
 				<div className="absolute bottom-0 left-0 right-0">
 					<div className="flex flex-wrap items-center justify-center text-white bg-black/80 px-3 sm:px-4 md:px-6 lg:px-8 py-2 sm:py-3 md:py-4 text-sm xs:text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
 						{transportModes.map((item, index) => (
@@ -30,7 +31,12 @@ const Banner = () => {
 								key={index}
 								className="flex items-center mx-2 sm:mx-3 md:mx-4 my-1"
 							>
-								<p className="font-extrabold">{item}</p>
+								<Link
+									to={`/transport/${item.toLowerCase()}`}
+									className="font-extrabold hover:underline transition"
+								>
+									{item}
+								</Link>
 								{index < transportModes.length - 1 && (
 									<div className="border-l h-4 sm:h-6 md:h-8 lg:h-10 border-gray-400 ml-2 sm:ml-3 md:ml-4"></div>
 								)}
