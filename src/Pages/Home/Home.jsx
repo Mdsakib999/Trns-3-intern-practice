7913bb9067bc49d39fcf126d1cab30f455538b46
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-05 13:36:37 +0600

    fixed the porblem

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,6 @@ import Opportunities from "../../Components/Shared/Opportunities/Opportunities";
 import Review from "../../Components/Shared/Review/Review";
 
 const Home = () => {
-<<<<<<< HEAD
 	return (
 		<div>
 			<Banner />
@@ -19,21 +18,6 @@ const Home = () => {
 			</div>
 		</div>
 	);
-=======
-  return (
-    <div>
-      <Banner />
-      <About />
-      <div data-aos="fade-right">
-        <Services />
-      </div>
-      <Review />
-      <div data-aos="fade-right">
-        <Opportunities />
-      </div>
-    </div>
-  );
->>>>>>> 2a1cb7aa193e3c7a0d2bceaeb3b794a5475f2368
 };
 
 export default Home;
